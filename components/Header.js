1933c8ed5572c67d830a1cb8086fe1d7a8b6d664
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2023-10-28 15:49:29 +0700

    UI header

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,24 @@ export default {
     return {};
   },
   template: `
-    <div>Header</div>
+    <div
+      class="d-flex justify-content-between align-items-center bg-bland-grey px-2 rounded"
+    >
+      <span>20120633</span>
+      <h5>Movies info</h5>
+      <div>
+        <div class="my-2 text-center">key</div>
+        <div class="my-2 form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="flexSwitchCheckDefault"
+          />
+          <label class="form-check-label" for="flexSwitchCheckDefault"
+            >Dark mode</label
+          >
+        </div>
+      </div>
+    </div>
     `,
 };
