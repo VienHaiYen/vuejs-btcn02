972e5f5f4c275ca4b0e51d1ba69d892d5d3e4c1d
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2023-10-28 15:09:21 +0700

    add layout

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
+import Header from "./components/Header.js";
+import Navbar from "./components/Navbar.js";
+import Main from "./components/Main.js";
+import Footer from "./components/Footer.js";
 export default {
+  components: {
+    Header,
+    Navbar,
+    Main,
+    Footer,
+  },
   data() {
     return { count: 0 };
   },
-  template: `<div>count is {{ count }}</div>`,
+  template: `
+    <Header />
+    <Navbar />
+    <Main />
+    <Footer />
+  `,
 };
