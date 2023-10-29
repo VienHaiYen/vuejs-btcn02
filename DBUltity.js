ae1d308a9207e4fcefaec7b42cff51a2f8d8ae5b
vhyenn <84668078+VienHaiYen@users.noreply.github.com>
2023-10-29 21:15:34 +0700

    update

--- DBUltity.js
+++ DBUltity.js
@@ -15,12 +15,22 @@ const DBUltity = {
   },
 };
 
+const getNNewestMovies = (movies, n) => {
+  let nLastestMovies = movies
+    .sort((a, b) => new Date(b.releaseDate) - new Date(a.releaseDate))
+    .splice(0, n);
+
+  return nLastestMovies;
+};
+
 const getData = (classN, pattern, params) => {
-  let perpage = 15;
+  let perpage = parseInt(params.get("perpage")) || 15;
   let page = parseInt(params.get("page")) || 1;
   let movies = [];
   if (classN == "all-movie") {
     movies = db.Movies.splice((page - 1) * perpage, page * perpage);
+  } else if (classN == "5-lastest") {
+    movies = getNNewestMovies(db.Movies, 5);
   } else if (classN == "topMovie") {
     movies = db.Top50Movies.splice((page - 1) * perpage, page * perpage);
   } else if (classN == "popular") {
@@ -32,8 +42,8 @@ const getData = (classN, pattern, params) => {
   return Promise.resolve({
     page,
     perpage,
-    totalpage,
-    number,
+    totalpage: Math.ceil(movies.length / perpage),
+    quantity: movies.length,
     movies,
   });
 };
